7ad7cff814ce7f96e80b28a91d6d3d5b8f318da4
usuario <usuario@curso.es>
2020-04-21 11:46:17 +0200

    Version 1

--- SparkSQL.py
+++ SparkSQL.py
@@ -15,9 +15,25 @@ sesion_spark = SparkSession.builder \
 datos_originales=sesion_spark.read\
     .format('com.databricks.spark.csv')\
     .options(header='true',inferschema='true')\
+    .option("sep",",")\
+    .option("quote","\"")\
+    .option("escape","\"")\
     .load('revisiones.csv')
 
 datos_originales.show()
+datos_originales.printSchema()
+
+datos_originales.select("overall").show()
+datos_originales.select(datos_originales['overall'], datos_originales['overall']+1).show()
+total_menores_3=datos_originales.select(datos_originales['overall'], datos_originales['overall']+1)\
+    .filter(datos_originales['overall'] <3).count()
+print(total_menores_3)
+
+
+datos_originales.groupBy("overall").count().show()
+
+datos_originales.createOrReplaceTempView("revisiones")
+sesion_spark.sql("SELECT reviewerID,asin,reviewerName,helpful FROM revisiones").show()
 
 # Cierro conexión Spark
 sesion_spark.stop()
